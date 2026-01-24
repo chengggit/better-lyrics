be85fe1edb714e6303d1678d84d9c778cd2c5d9e
Adalie Ahuja <hi@adalie.me>
2026-01-24 18:21:21 -0500

    remove unused exports

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -18,7 +18,6 @@ export const ROMANIZED_LYRICS_CLASS = "blyrics--romanized" as const;
 export const FOOTER_CLASS = "blyrics-footer" as const;
 
 // DOM Selectors
-export const SONG_IMAGE_SELECTOR = "#song-image>#thumbnail>#img" as const;
 export const TAB_RENDERER_SELECTOR = "#tab-renderer" as const;
 export const NO_LYRICS_TEXT_SELECTOR =
   "#tab-renderer > ytmusic-message-renderer > yt-formatted-string.text.style-scope.ytmusic-message-renderer" as const;
@@ -123,9 +122,6 @@ export const FOOTER_NOT_VISIBLE_LOG =
 export const LYRICS_TAB_NOT_DISABLED_LOG =
   "[BetterLyrics] (Safe to ignore) Lyrics tab is not disabled, unable to enable it" as const;
 export const SONG_SWITCHED_LOG = "[BetterLyrics] Song has been switched" as const;
-export const ALBUM_ART_ADDED_LOG = "[BetterLyrics] Album art added to the layout" as const;
-export const ALBUM_ART_SIZE_CHANGED = "[BetterLyrics] Album art size changed to" as const;
-export const ALBUM_ART_REMOVED_LOG = "[BetterLyrics] Album art removed from the layout" as const;
 export const LOADER_TRANSITION_ENDED = "[BetterLyrics] Loader Transition Ended" as const;
 export const LOADER_ANIMATION_END_FAILED = "[BetterLyrics] Loader Animation Didn't End" as const;
 export const PAUSING_LYRICS_SCROLL_LOG = "[BetterLyrics] Pausing Lyrics Autoscroll Due to User Scroll" as const;
